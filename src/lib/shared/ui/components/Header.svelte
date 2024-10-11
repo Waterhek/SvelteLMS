5d8b46b287a1f905a11b9080efd98123ee01b010
Waterfaku <103442474+Waterfaku@users.noreply.github.com>
2024-10-11 18:03:51 +0800

    need modify

---
+++ src/lib/shared/ui/components/Header.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+    export let showSidebar: boolean; // Sidebar visibility is passed as a prop
+
+    // Dispatch an event to toggle sidebar visibility
+    const dispatch = createEventDispatcher();
+
+    function handleToggle() {
+        dispatch('toggleSidebar');
+    }
+</script>
+
+<div class="header">
+    <div class="container-fluid">
+        <div class="header-content">
+            <button class="btn" type="button" on:click={handleToggle}>
+                <i class="ri-menu-line"></i>
+            </button>
+
+            <ul class="navbar-nav">
+                <div class="profile-container" data-bs-toggle="dropdown" data-bs-target="#profile-dropdown">
+                    <img src="favicon.png" alt="avatar" />
+                </div>
+                <!-- Dummy -->
+                <div class="dropdown-menu dropdown-menu-end"  id="profile-dropdown">
+                    <div class="card">
+                        <div class="card-header">
+                            Chin Chee Onn
+                        </div>
+                        <div class="card-body">
+                            <ul class="dropdown">
+                                <li><a class="dropdown-item" href="#">Menu item</a></li>
+                                <li><a class="dropdown-item" href="#">Menu item</a></li>
+                                <li><a class="dropdown-item" href="#">Menu item</a></li>
+                            </ul>
+                        </div>
+                        <div class="card-footer"></div>
+                    </div>
+                </div>
+                <!-- End Dummy -->
+            </ul>
+        </div>
+    </div>
+</div>
+
+<style>
+    .header{
+        border-bottom:  1px solid grey;
+        padding: 10px 0;
+    }
+
+    .header-content{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .profile-container img{
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+    }
+
+    .dropdown-menu{
+        padding: 0;
+        border: none;
+    }
+</style>
---
+++ src/lib/shared/ui/components/Sidebar.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+    import "remixicon/fonts/remixicon.css";
+    import { page } from '$app/stores';
+
+    $: currentRoute = $page.url.pathname;
+
+    export let isOpen: boolean = true;
+</script>
+
+<div class="sidebar {isOpen ? 'open' : 'closed'}">
+    <div class="logo">
+        <img src="dummy/logo.png" alt="logo" />
+    </div>
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link {currentRoute.startsWith('/student') ? 'active' : ''}" href="/student">
+                <i class="ri-graduation-cap-fill"></i>
+               <span>Student</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link {currentRoute.startsWith('/teacher') ? 'active' : ''}" href="/teacher">
+                <i class="ri-skip-down-fill"></i>
+                <span>Teacher</span>
+            </a>
+        </li>
+    </ul>
+</div>
+
+<style>
+    .sidebar{
+        position: fixed;
+        width: 18rem;
+        height: 100vh;
+        background-color: var(--bs-primary-color);
+        padding: 30px 20px;
+        transition: all 0.5s ease;
+    }
+
+    :global(.sidebar.closed) {
+        transform: translateX(-100%);
+        width: 0;
+        overflow: hidden;
+    }
+
+    :global(.sidebar.open) {
+        transform: translateX(0);
+        width: 18rem;
+    }
+
+    .logo{
+        margin-bottom: 30px;
+    }
+
+    .logo img{
+        width: 100%;
+        height: 70px;
+    }
+
+    .nav-item{
+        list-style: none;
+        margin-bottom: 10px;
+    }
+
+    .nav-link{
+        color: white;
+        padding: 10px 20px;
+        border-radius: 5px;
+    }
+
+    .nav-link i{
+        margin-right: 10px;
+    }
+
+    .nav-link span{
+        font-size: 0.9em;
+    }
+
+    .nav-link.active{
+        background-color: grey;
+    }
+</style>
