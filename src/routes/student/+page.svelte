39472fed2097c1da9f7f841a98c2dd3c9295f2ae
Waterfaku <103442474+Waterfaku@users.noreply.github.com>
2024-10-11 18:03:28 +0800

    header, sidebar, layout need to modify again

---
+++ src/routes/student/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import ManagementLayout from "$lib/shared/layouts/ManagementLayout.svelte";
+
+</script>
+
+<div class="container-fluid">
+    <ManagementLayout title="Students" createButtonLabel="New Student" createButtonUrl="/student/create" />
+</div>
